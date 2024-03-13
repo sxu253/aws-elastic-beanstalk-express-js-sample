2c8d60b3a913036da2f7131f1207227011bccdb8
Sonia Xu <sonix@amazon.com>
2024-03-13 05:41:17 +0000

    Update messaging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('You got this, goodnight World!'));
+app.get('/', (req, res) => res.send('Goodnight World! You got this.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
